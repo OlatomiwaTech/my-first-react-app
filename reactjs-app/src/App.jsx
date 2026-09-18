4380771d96d5bbad19a0d3e007557e6031926f35
OlatomiwaTech <mrcodex2012@gmail.com>
2026-09-18 22:47:21 +0100

    polished the set count feature

--- reactjs-app/src/App.jsx
+++ reactjs-app/src/App.jsx
@@ -1,24 +1,33 @@
-
 import { useState, useEffect } from "react";
+
 import "./App.css";
 
 function App() {
   const [hasLiked, setHasLiked] = useState(false);
-  useEffect(effect: () => {
-    console.log('you liked the workspace')
-  })
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (hasLiked) {
+      console.log("you liked the workspace");
+    }
+  }, [hasLiked]);
+
+  const handleLike = () => {
+    setHasLiked(!hasLiked);
+    setCount(hasLiked ? count - 1 : count + 1);
+  };
+
   return (
     <div className="card">
       <h2>Premium Workspace</h2>
 
       <button className="get-started">Get Started</button>
 
-      <button onClick={() => setHasLiked(!hasLiked)} className="like-btn">
-        {hasLiked ? "❤️" : "🤍"}
+      <button onClick={handleLike} className="like-btn">
+        {hasLiked ? "❤️" : "🤍"} {count}
       </button>
     </div>
   );
 }
 
 export default App;
-
