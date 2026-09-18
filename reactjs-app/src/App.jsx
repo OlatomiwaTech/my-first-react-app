080bcf428404b388d293f275f2f015c5d2f16867
OlatomiwaTech <mrcodex2012@gmail.com>
2026-09-18 21:27:46 +0100

    Changed the background color to aqua

--- reactjs-app/src/App.jsx
+++ reactjs-app/src/App.jsx
@@ -10,7 +10,7 @@ import './App.css'
 
 function App() {
 return (
-  <div cla>
+  <div class='card'>
     <h2>Functional Statement</h2>
     <Card title="Card Title" description="This is a description of the card." />
   </div>
