fe066a154eae8ff345ee40a6450a59b67390c4a6
OlatomiwaTech <mrcodex2012@gmail.com>
2026-09-18 22:21:17 +0100

    Fixed and polished the Liked and hasLiked feature

--- reactjs-app/src/App.jsx
+++ reactjs-app/src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
-import './App.css'
 
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [hasLiked, sethasLiked] = useState(false);
-return (
- <div class='card'>
-  <h2>Premium Workspace</h2>
-  <button class='get-started'>Get-Started</button>
-  <button onClick={() => sethasLiked(value )}>Like</button>
- </div>
-) 
+  const [hasLiked, setHasLiked] = useState(false);
+
+  return (
+    <div className="card">
+      <h2>Premium Workspace</h2>
+
+      <button className="get-started">Get Started</button>
+
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? "Liked" : "Like"}
+      </button>
+    </div>
+  );
 }
-export default App
+
+export default App;
+
